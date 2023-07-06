9b2e930732084fcbe74ccc092fd0b8a5467769fb
Adrian Medynski <128226981+Medyk83@users.noreply.github.com>
2023-07-06 22:20:49 +0200

    Adrian
    Adrian

---
+++ Zad1/Program.cs
@@ -0,0 +1,36 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+class Program
+{
+    static void Main()
+    {
+
+        Console.WriteLine("Podaj czas w sekundach:");
+        int czaswsekundach;
+
+        while (!int.TryParse(Console.ReadLine(), out czaswsekundach) || czaswsekundach <= 0)
+        {
+            Console.WriteLine("Niepoprawna wartość. Podaj poprawną wartość czasu:");
+        }
+        int czas = czaswsekundach;
+        string sformatowanyCzas = FormatujCzas(czas);
+        Console.WriteLine("Sformatowany Czas to: " + sformatowanyCzas);
+        Console.ReadLine();
+    }
+
+    static string FormatujCzas(int czas)
+    {
+        int godziny = czas / 3600;
+        int minuty = (czas % 3600) / 60;
+        int sekundy = (czas % 3600) % 60;
+
+        string sGodziny = godziny.ToString("D2");
+        string sMinuty = minuty.ToString("D2");
+        string sSekundy = sekundy.ToString("D2");
+
+        string sformatowanyCzas = $"{sGodziny}:{sMinuty}:{sSekundy}";
+
+        return sformatowanyCzas;
+    }
+}
---
+++ Zad2/Program.cs
@@ -0,0 +1,47 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Podaj współrzędne punktu początkowego (x1, y1):");
+        double x1 = WczytajLiczbeRzeczywista("x1");
+        double y1 = WczytajLiczbeRzeczywista("y1");
+
+        Console.WriteLine("Podaj współrzędne punktu końcowego (x2, y2):");
+        double x2 = WczytajLiczbeRzeczywista("x2");
+        double y2 = WczytajLiczbeRzeczywista("y2");
+
+        double dlugoscOdcinka = ObliczDlugoscOdcinka(x1, y1, x2, y2);
+
+        Console.WriteLine($"Długość odcinka wynosi: {dlugoscOdcinka}");
+        Console.ReadLine();
+    }
+
+    static double WczytajLiczbeRzeczywista(string nazwa)
+    {
+        double liczba;
+        bool poprawnaWartosc;
+
+        do
+        {
+            Console.Write($"Podaj wartość dla {nazwa}: ");
+            poprawnaWartosc = double.TryParse(Console.ReadLine(), out liczba);
+
+            if (!poprawnaWartosc)
+            {
+                Console.WriteLine("Niepoprawna wartość. Podaj poprawną liczbę rzeczywistą.");
+            }
+
+        } while (!poprawnaWartosc);
+
+        return liczba;
+    }
+
+    static double ObliczDlugoscOdcinka(double x1, double y1, double x2, double y2)
+    {
+        double dlugosc = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
+        return dlugosc;
+    }
+}
